669528b7b03fe41495ad29bef6633e06e8039bc7
Lionello Lunesu <lio+git@lunesu.com>
2024-05-25 09:02:42 -0700

    check headers in individual Go files

--- main.go
+++ main.go
@@ -71,6 +71,103 @@ func normalizeImportPath(importPath string) ImportPath {
 	return ImportPath(strings.TrimPrefix(importPath, "vendor/"))
 }
 
+var licenseIdCache = map[string]string{} // file -> license ID mapping
+
+func (p *Package) findLicense() (string, error) {
+	if p.license != "" {
+		return p.license, nil
+	}
+	if p.Standard {
+		return "standard", nil
+	}
+	if p.ForTest != "" {
+		return "test", nil
+	}
+
+	// Check whether (all) the source files contain a license header
+	licenseId, err := findLicenseHeaders(p.Dir, p.GoFiles)
+	if err != nil {
+		// Look for a LICENSE* file in the package directory (or parents) instead
+		licenseFile, err := findLicenseFileUp(p.Dir)
+		if err != nil {
+			return "", errors.Wrapf(err, "finding license file for %s", p.ImportPath)
+		}
+
+		licenseId = licenseIdCache[licenseFile]
+		if licenseId == "" { // not in cache
+			licenseId, err = ReadLicenseFile(licenseFile)
+			if err != nil {
+				return "", err
+			}
+			licenseIdCache[licenseFile] = licenseId
+		}
+	}
+
+	p.license = licenseId
+	return licenseId, nil
+}
+
+func findLicenseHeaders(dir string, files []string) (string, error) {
+	licenseIds := map[string]int{}
+	for _, file := range files {
+		license, err := ReadLicenseFile(filepath.Join(dir, file))
+		if err != nil {
+			return "", err // bail on first error (eg. file without license)
+		}
+		licenseIds[license]++
+	}
+	for licenseId, _ := range licenseIds {
+		return licenseId, nil // TODO: handle multiple licenses
+	}
+	return "", ErrNoLicense
+}
+
+func ReadLicenseFile(licenseFile string) (string, error) {
+	license, err := os.ReadFile(licenseFile)
+	if err != nil {
+		return "", errors.Wrapf(err, "reading license file %s", licenseFile)
+	}
+	cov := licensecheck.Scan(license)
+	if len(cov.Match) == 0 {
+		return "", errors.Wrapf(ErrNoLicense, "scanning license file %s", licenseFile)
+	}
+	return cov.Match[0].ID, nil // TODO: handle multiple licenses
+}
+
+// Package represents a Go package. This (partial) definition is copied from the `go help list` command.
+type Package struct {
+	Dir        string   // directory containing package sources
+	ImportPath string   // import path of package in dir
+	Imports    []string // import paths used by this package
+	ForTest    string   // package is only for use in named test
+	Deps       []string // all (recursively) imported dependencies
+	Standard   bool     // is this package part of the standard Go library?
+	GoFiles    []string // .go source files (excluding CgoFiles, TestGoFiles, XTestGoFiles)
+
+	license string
+}
+
+// getPackageDependencies returns a list of dependencies for the current module, including their paths and directories
+func getPackageDependencies() ([]Package, error) {
+	out, err := exec.Command("go", "list", "-deps", "-json").Output()
+	if err != nil {
+		return nil, err
+	}
+
+	decoder := json.NewDecoder(strings.NewReader(string(out)))
+	var packages []Package
+
+	for {
+		var mod Package
+		if err := decoder.Decode(&mod); err != nil {
+			break
+		}
+		packages = append(packages, mod)
+	}
+
+	return packages, nil
+}
+
 func main() {
 	// Step 1: Get the list of dependencies
 	deps, err := getPackageDependencies()
@@ -126,68 +223,4 @@ func main() {
 			}
 		}
 	}
-}
-
-var licenseIds = map[string]string{} // file -> license ID mapping
-
-func (p *Package) findLicense() (string, error) {
-	if p.license != "" || p.Standard || p.ForTest != "" {
-		return p.license, nil
-	}
-
-	licenseFile, err := findLicenseFileUp(p.Dir)
-	if err != nil {
-		return "", errors.Wrapf(err, "finding license file for %s", p.ImportPath)
-	}
-
-	licenseId := licenseIds[licenseFile]
-	if licenseId == "" {
-		license, err := os.ReadFile(licenseFile)
-		if err != nil {
-			return "", errors.Wrapf(err, "reading license file %s", licenseFile)
-		}
-		cov := licensecheck.Scan(license)
-		if cov.Percent > 85 {
-			licenseId = cov.Match[0].ID
-		} else {
-			return "", fmt.Errorf("license coverage too low for %s: %v", licenseFile, cov)
-		}
-		licenseIds[licenseFile] = licenseId
-	}
-
-	p.license = licenseId
-	return p.license, nil
-}
-
-// Package represents a Go package. This (partial) definition is copied from the `go help list` command.
-type Package struct {
-	Dir        string   // directory containing package sources
-	ImportPath string   // import path of package in dir
-	Imports    []string // import paths used by this package
-	ForTest    string   // package is only for use in named test
-	Deps       []string // all (recursively) imported dependencies
-	Standard   bool     // is this package part of the standard Go library?
-
-	license string
-}
-
-// getPackageDependencies returns a list of dependencies for the current module, including their paths and directories
-func getPackageDependencies() ([]Package, error) {
-	out, err := exec.Command("go", "list", "-deps", "-json").Output()
-	if err != nil {
-		return nil, err
-	}
-
-	decoder := json.NewDecoder(strings.NewReader(string(out)))
-	var packages []Package
-
-	for {
-		var mod Package
-		if err := decoder.Decode(&mod); err != nil {
-			break
-		}
-		packages = append(packages, mod)
-	}
-
-	return packages, nil
 }
